7d1284f4a2ec43affa734792dc8886e18a284ed6
Vivek Basavarajappa <vivek.basavaraj123@gmail.com>
2017-10-02 00:54:56 -0500

    feat: card is opened upon clicking
    Whenever is card is clicked, it opens and again when it is clicked, it closes.

--- js/app.js
+++ js/app.js
@@ -25,7 +25,6 @@ function shuffle(array) {
     return array;
 }
 
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -36,3 +35,7 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+$('.card').on('click',function(){
+    $(this).toggleClass( "open show");
+});
